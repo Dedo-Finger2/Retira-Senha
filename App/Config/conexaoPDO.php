7b4f6f0598ac25e144743dcfbb27ecdb17638b80
Dedo_Finger2 <antonioimportant@gmail.com>
2023-06-15 12:34:32 -0300

    new folders

---
+++ App/Config/conexaoPDO.php
@@ -0,0 +1,16 @@
+<?php
+
+const DATA_LAYER_CONFIG = [
+    "driver" => "mysql", // MariaDB usa o mesmo driver do mysql
+    "host" => "localhost", // O host do banco
+    "port" => "3306", // A porta do banco
+    "dbname" => "atende", // O nome do banco
+    "username" => "root", // O username
+    "passwd" => "", // A senha
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", // Charset que usamos
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // se der exceção vai estar guardado aqui
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, // todos os resultado do banco vem dentro de um objeto
+        PDO::ATTR_CASE => PDO::CASE_NATURAL // a conexão não vai fazer conversão de caracteres
+    ]
+];
