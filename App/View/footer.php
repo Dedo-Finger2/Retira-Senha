a47a2987ae99141d9d45ea53a4c217a07455c8cf
Dedo_Finger2 <antonioimportant@gmail.com>
2023-06-22 20:02:01 -0300

    nome e link footer done

--- App/View/footer.php
+++ App/View/footer.php
@@ -3,8 +3,8 @@
 <footer class="bg-light text-center text-lg-start fixed-bottom">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    © 2023 Copyright:
+    <a class="text-dark" href="#">Retira Senha</a>
   </div>
   <!-- Copyright -->
 </footer>
