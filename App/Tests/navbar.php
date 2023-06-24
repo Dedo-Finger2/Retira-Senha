ddfd23541c1c2b9bb8681dfbaa3de5064200d3a4
Dedo_Finger2 <antonioimportant@gmail.com>
2023-06-24 11:12:01 -0300

    home quase pronta

---
+++ App/Tests/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Retira Senha</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <!-- O action desse formulário tem que ir para o Controller/cadastroHandler.php -->
+            <form action="#" method="post">
+                <button type="button" class="btn btn-danger">Log-off</button>
+            </form>
+        </div>
+    </div>
+</nav>
