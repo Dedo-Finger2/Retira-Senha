551b4408867112d4d569bf59ce18c1562e7c3770
Dedo_Finger2 <antonioimportant@gmail.com>
2023-06-24 11:14:21 -0300

    footer pronto2

--- App/View/footer.php
+++ App/View/footer.php
@@ -1,10 +1,10 @@
 <!-- Link css -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-<footer class="bg-light text-center text-lg-start fixed-bottom">
+<footer class="bg-dark text-center text-lg-start fixed-bottom">
   <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="text-center p-3 text-light">
     © 2023 Copyright:
-    <a class="text-dark" href="#">Retira Senha</a>
+    <a class="text-light" href="#">Retira Senha</a>
   </div>
   <!-- Copyright -->
 </footer>
