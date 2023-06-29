5f366b6d5ebe6876ee2e7760ccf75631e00330ae
Dedo_Finger2 <antonioimportant@gmail.com>
2023-06-28 21:18:26 -0300

    ultimas modificações do sistema

--- App/Controller/tabelaFiltragemDeSenhas.php
+++ App/Controller/tabelaFiltragemDeSenhas.php
@@ -12,92 +12,88 @@ $idadeMinima = (new ControllerTurma())->getIdadesMinimas();
 $idadeMaxima = (new ControllerTurma())->getIdadesMaximas();
 
 ?>
-    <div class="container">
+    <div class="container mt-5">
         <div class="card">
             <div class="card-body">
+                <h1>Senhas disponiveis</h1>
+                <form action="senhasFiltradas.php" method="post">
+                    <label>Turno</label> <!-- "Nome do input" - Greg -->
+                    <select name="turno"> <!-- "Select" - Greg -->
+                        <?php
+                            foreach ($turnos as $turno) {
+                                ?>
+                                    <option value="<?= $turno ?>"><?=$turno?></option> <!-- "Dados do banco serão inseridos aqui sempre dentro de um <option></option>" - Greg -->
+                                <?php
+                            }
+                        ?>
+                    </select>
+                    <br><br>
+                    <label>Faixa etária de</label>
+                    <select name="idadeMinima">
+                        <?php
+                            foreach ($idadeMinima as $idade) {
+                                ?>
+                                    <option value="<?= $idade ?>"><?=$idade?></option>
+                                <?php
+                            }
+                        ?>
+                    </select>
+                    anos até
+                    <select name="idadeMaxima">
+                        <?php
+                            foreach ($idadeMaxima as $idade) {
+                                ?>
+                                    <option value="<?= $idade ?>"><?=$idade?></option>
+                                <?php
+                            }
+                        ?>
+                    </select>
+                    anos
+                    <br><br> <!-- "Quebra de linha pq não aguentei o agrupamento disso tudo kk" - Greg -->
+                    
+                    <!--
+                        "O nome desses checkbox é o mesmo porque assim eles são tratados como um array!
+                        Com isso vamos poder saber quais dias da semana o usuário escolheu e filtrar as senhas" - Greg
+                    -->
 
-            <h1>Senhas disponiveis</h1>
-<form action="senhasFiltradas.php" method="post">
+                    <!-- Checkbox -->
+                                
+                    <div class="col-md-6">
+                    <h3>Dias da semana:</h3>
 
-    <label>Turno</label> <!-- "Nome do input" - Greg -->
-    <select name="turno"> <!-- "Select" - Greg -->
-        <?php
-            foreach ($turnos as $turno) {
-                ?>
-                    <option value="<?= $turno ?>"><?=$turno?></option> <!-- "Dados do banco serão inseridos aqui sempre dentro de um <option></option>" - Greg -->
-                <?php
-            }
-        ?>
-    </select>
+                    <div class="form-check form-check-inline">
+                        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox1" value="SEGUNDA">
+                        <label class="form-check-label" for="inlineCheckbox1">Segunda-feira</label>
+                    </div>
 
-    <label>Faixa etária</label>
-    De
-    <select name="idadeMinima">
-        <?php
-            foreach ($idadeMinima as $idade) {
-                ?>
-                    <option value="<?= $idade ?>"><?=$idade?></option>
-                <?php
-            }
-        ?>
-    </select>
-    anos até
-    <select name="idadeMaxima">
-        <?php
-            foreach ($idadeMaxima as $idade) {
-                ?>
-                    <option value="<?= $idade ?>"><?=$idade?></option>
-                <?php
-            }
-        ?>
-    </select>
+                    <div class="form-check form-check-inline">
+                        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox2" value="TERÇA">
+                        <label class="form-check-label" for="inlineCheckbox2">Terça-feira</label>
+                    </div>
 
-    <br><br> <!-- "Quebra de linha pq não aguentei o agrupamento disso tudo kk" - Greg -->
-    
-    <!--
-        "O nome desses checkbox é o mesmo porque assim eles são tratados como um array!
-        Com isso vamos poder saber quais dias da semana o usuário escolheu e filtrar as senhas" - Greg
-    -->
+                    <div class="form-check form-check-inline">
+                        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox3" value="QUARTA">
+                        <label class="form-check-label" for="inlineCheckbox3">Quarta-feira</label>
+                    </div>
+                    <br>
+                    <div class="form-check form-check-inline">
+                        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox4" value="QUINTA">
+                        <label class="form-check-label" for="inlineCheckbox4">Quinta-feira</label>
+                    </div>
 
-    <!-- Checkbox -->
-                
-    <div class="col-md-6">
-    <h3>Dias da semana:</h3>
+                    <div class="form-check form-check-inline">
+                        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox5" value="SEXTA">
+                        <label class="form-check-label" for="inlineCheckbox5">Sexta-feira</label>
+                    </div>
 
-    <div class="form-check form-check-inline">
-        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox1" value="SEGUNDA">
-        <label class="form-check-label" for="inlineCheckbox1">Segunda-feira</label>
-    </div>
-
-    <div class="form-check form-check-inline">
-        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox2" value="TERÇA">
-        <label class="form-check-label" for="inlineCheckbox2">Terça-feira</label>
-    </div>
-
-    <div class="form-check form-check-inline">
-        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox3" value="QUARTA">
-        <label class="form-check-label" for="inlineCheckbox3">Quarta-feira</label>
-    </div>
-
-    <div class="form-check form-check-inline">
-        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox4" value="QUINTA">
-        <label class="form-check-label" for="inlineCheckbox4">Quinta-feira</label>
-    </div>
-
-    <div class="form-check form-check-inline">
-        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox5" value="SEXTA">
-        <label class="form-check-label" for="inlineCheckbox5">Sexta-feira</label>
-    </div>
-
-    <div class="form-check form-check-inline">
-        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox6" value="SÁBADO">
-        <label class="form-check-label" for="inlineCheckbox6">Sábado</label>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Filtrar senhas</button>
-    
+                    <div class="form-check form-check-inline">
+                        <input name="dias[]" class="form-check-input" type="checkbox" id="inlineCheckbox6" value="SÁBADO">
+                        <label class="form-check-label" for="inlineCheckbox6">Sábado</label>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Filtrar senhas</button> 
+                </form>
             </div>
         </div>
     </div>
-    </form>
-    </div>
+</div>
--- App/View/filtragemDeSenhas.php
+++ App/View/filtragemDeSenhas.php
@@ -24,7 +24,6 @@ $exibirModal = isset($_GET['modal']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Public/Css/style.css">
     <link rel="stylesheet" href="../Public/Css/customBootstrap.css">
     <title>Filtragem de senhas</title>
 
