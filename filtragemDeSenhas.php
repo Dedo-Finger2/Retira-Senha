908d2acf0f02445260329d7a95249dbd19576ffa
Joice-Almeida <127358733+Joice-Almeida@users.noreply.github.com>
2023-06-28 21:03:40 -0300

    modificações de arquivos
    novas modificações no sistema by Chatinha.

---
+++ filtragemDeSenhas.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * "Inicia a sessão e verifica
+ * Se a variável de sessão RG for varzia, significa que o usuário é sabido demais e ta tentando acessar essa página
+ * sem se logar antes! Se for caso, então mandar ele de volta pra tela de login" - Greg
+ * Comentei o sistema de verificação só pra ver a tela enquanto edito - leone
+ */
+session_start();
+
+if (!isset($_SESSION['rg'])) {
+    header('Location: login.php');
+    exit();
+}
+
+$exibirModal = isset($_GET['modal']);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Public/Css/style.css">
+    <link rel="stylesheet" href="../Public/Css/customBootstrap.css">
+    <title>Filtragem de senhas</title>
+
+    <!--NAVBAR PROVISÓRIA-->
+    <?php require_once("finalNavbar.php"); ?>
+</head>
+
+<body>
+  
+    <?php require_once("../Controller/tabelaFiltragemDeSenhas.php") ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+        crossorigin="anonymous"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            if (<?php echo isset($_GET['senhaObtida']) ? 'true' : 'false'; ?>) {
+                window.alert('Senha obtida com sucesso!');
+            } if (<?php echo isset($_GET['error']) ? 'true' : 'false'; ?>) {
+                window.alert('Ocorreu um erro!');
+            }
+        });
+    </script>
+</body>
+
+</html>
--- navbar.php
+++ navbar.php
@@ -1,28 +1,12 @@
-<nav class="navbar navbar-expand-lg bg-nosso-azul border-bottom border-bottom-dark" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Retira Senha</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="filtragemDeSenhas.php">Vagas disponiveis</a>
-                </li>
-            </ul>
-            <!-- O action desse formulário tem que ir para o Controller/cadastroHandler.php -->
-            <form action="#" method="post">
-                <button type="button" class="btn btn-danger">Log-off</button>
-            </form>
-        </div>
-    </div>
+<!-- "A barra de navegação que vai ser incluída nas demais telas deve ser feita aqui" - Greg -->
+<nav>
+    <a href="login.php">Login</a> - 
+    <a href="cadastro.php">Cadastro</a> -
+    <a href="index.php">Suas senhas</a> -
+    <a href="filtragemDeSenhas.php">Filtragem de senhas</a> -
+    <a href="senhasFiltradas.php">Senhas filtradas</a>
+    <form action="../Controller/cadastroHandler.php" method="post">
+        <button type="submit">Log-Off</button>  
+    </form>
 </nav>
-<!-- Scripts do Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+<hr>
