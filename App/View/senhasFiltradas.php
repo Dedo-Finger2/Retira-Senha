2628d57df6dd864ecfb6210522c6581d213b1eaf
joicec.almeida96@gmail.com <joicec.almeida96@gmail.com>
2023-06-29 20:40:31 -0300

    novas alterações

--- App/View/senhasFiltradas.php
+++ App/View/senhasFiltradas.php
@@ -19,16 +19,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../Public/Css/customBootstrap.css">
         <title>Senhas Filtradas</title>
         <!--NAVBAR PROVISÓRIA-->
-        <?php require_once("navbar.php"); ?>
+        <?php require_once("finalNavbar.php"); ?>
     </head>
     <body>
         
         <!-- TABELA PROVISÓRIA -->
         <div class="container">
-            <h1 class="text-center">Senhas filtradas</h1>
+            <h1 class="text-center mt-5">Escolha sua senha</h1>
+            <h2 class="text-center">Aqui você poderá escolher senhas para o curso desejado.</h2>
             <?php require_once("../Controller/tabelaSenhasFiltradas.php") ?>
         </div>
+        <?php require_once("footer.php");?>
     </body>
 </html>
